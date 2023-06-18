ff5e4ac462f0fd376a4147dfe64d6a12cf5ab78e
BenMukebo <l.benkasmukebo7@gmail.com>
2023-06-18 17:47:15 +0200

    Add color change logic

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ button.innerText = 'CLICK ME';
 h1.appendChild(span);
 body.appendChild(h1);
 body.appendChild(button);
+
+button.addEventListener('click', () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  body.style.backgroundColor = '#' + randomColor;
+  span.innerText = '#' + randomColor;
+});
